1fae975c39b2a3667bca87a278004487cd4c4e1d
Ken Zhang <kyz211@nyu.edu>
2015-10-22 02:31:36 -0400

    initial rss dump

--- home.php
+++ home.php
@@ -11,8 +11,39 @@
     overflow: hidden;
   }
 
-  body {
-    font: 100% / 1.5 Arvo;
+  * {
+    font-family: 'Arvo', sans-serif;
+    margin: 0;
+    text-decoration: none;
+  }
+  
+  a:link    {color:white;}  
+  a:visited {color:white;}  
+  a:hover   {color:gray;} 
+  a:active  {color:#000;}
+  .forms {
+    position:absolute;
+    top: 4%;
+    left: 54%;
+    margin-left: -110px;
+    color: black;
+    z-index: 100;
+    font-size: 23px;
+      display:inline-block;
+  }
+  #updateClock {
+    position:absolute;
+    top: 9%;
+    left: 50%;
+    margin-left: -150px;
+    z-index: 100;
+  }
+  #rss {
+    z-index: 100;
+    position: absolute;
+    left: 50%;
+      top: 12%;
+      margin-left: -550px;
   }
 </style>
 <script>
@@ -36,5 +67,73 @@ ga('send', 'pageview');
 <!-- End Google Analytics -->
 </head>
 <body>
+  <div class = "forms">
+    Welcome.
+  </div>
+  <div id = "updateClock">
+    <script>
+    //clock
+    var mydate = new Date();
+    var hours = mydate.getHours();
+    var minutes = mydate.getMinutes();
+
+    var year = mydate.getYear()
+    if (year < 1000)
+    year += 1900
+    var day = mydate.getDay()
+    var month = mydate.getMonth()
+    var daym = mydate.getDate()
+    if (daym < 10)
+    daym = "0" + daym
+    var dayarray = new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday")
+    var montharray = new Array("January","February","March","April","May","June","July","August","September","October","November","December")
+    if(hours > 12) {
+      if(minutes >= 10) {
+      document.write("<small><font color='000000' face='Arial'><b>"+"Last Updated " +(hours - 12)+":"+minutes+"pm "+ dayarray[day]+", "+montharray[month]+" "+daym+", "+year+"</b></font></small>");
+      }
+      else {
+      document.write("<small><font color='000000' face='Arial'><b>"+"Last Updated " +(hours - 12)+":0"+minutes+"pm "+ dayarray[day]+", "+montharray[month]+" "+daym+", "+year+"</b></font></small>");
+      }
+    }
+    else if(hours == 12) {
+      if(minutes >= 10) {
+      document.write("<small><font color='000000' face='Arial'><b>"+"Last Updated " +hours+":"+minutes+"pm "+ dayarray[day]+", "+montharray[month]+" "+daym+", "+year+"</b></font></small>");
+      }
+      else {
+      document.write("<small><font color='000000' face='Arial'><b>"+"Last Updated " +hours+":0"+minutes+"pm "+ dayarray[day]+", "+montharray[month]+" "+daym+", "+year+"</b></font></small>");
+      }
+    }
+    else if(hours == 0) {
+      if(minutes >= 10) {
+      document.write("<small><font color='000000' face='Arial'><b>"+"Last Updated " +(hours+12)+":"+minutes+"am "+ dayarray[day]+", "+montharray[month]+" "+daym+", "+year+"</b></font></small>");
+      }
+      else {
+      document.write("<small><font color='000000' face='Arial'><b>"+"Last Updated " +(hours+12)+":0"+minutes+"am "+ dayarray[day]+", "+montharray[month]+" "+daym+", "+year+"</b></font></small>");
+      }
+    }
+    else {
+      if(minutes >= 10) {
+      document.write("<small><font color='000000' face='Arial'><b>"+"Last Updated " +hours+":"+minutes+"am "+ dayarray[day]+", "+montharray[month]+" "+daym+", "+year+"</b></font></small>");
+      }
+      else {
+      document.write("<small><font color='000000' face='Arial'><b>"+"Last Updated " +hours+":0"+minutes+"am "+ dayarray[day]+", "+montharray[month]+" "+daym+", "+year+"</b></font></small>");
+      }
+    }
+    
+    </script>
+  </div>
+  <div id = "rss">
+    <script type="text/javascript">document.write('\x3Cscript type="text/javascript" src="' + ('https:' == document.location.protocol ? 'https://' : 'http://') + 'feed.mikle.com/js/rssmikle.js">\x3C/script>');</script><script type="text/javascript">(function() {var params = {rssmikle_url: "http://feeds.feedburner.com/TechCrunch/",rssmikle_frame_width: "110%",rssmikle_frame_height: "500",frame_height_by_article: "3",rssmikle_target: "_blank",rssmikle_font: "Verdana, Arial",rssmikle_font_size: "12",rssmikle_border: "off",responsive: "off",rssmikle_css_url: "",text_align: "left",text_align2: "left",corner: "off",scrollbar: "on",autoscroll: "off",scrolldirection: "up",scrollstep: "3",mcspeed: "20",sort: "Off",rssmikle_title: "on",rssmikle_title_sentence: "",rssmikle_title_link: "",rssmikle_title_bgcolor: "#000000",rssmikle_title_color: "#FFFFFF",rssmikle_title_bgimage: "",rssmikle_item_bgcolor: "#FFFFFF",rssmikle_item_bgimage: "",rssmikle_item_title_length: "150",rssmikle_item_title_color: "#000000",rssmikle_item_border_bottom: "on",rssmikle_item_description: "on",item_link: "off",rssmikle_item_description_length: "750",rssmikle_item_description_color: "#666666",rssmikle_item_date: "gl1",rssmikle_timezone: "Etc/GMT",datetime_format: "%b %e, %Y %l:%M:%S %p",item_description_style: "text+tn",item_thumbnail: "full",item_thumbnail_selection: "auto",article_num: "15",rssmikle_item_podcast: "off",keyword_inc: "",keyword_exc: ""};feedwind_show_widget_iframe(params);})();</script><div style="font-size:10px; text-align:center; width:1000px;"></div><br/>
+
+    <script type="text/javascript">document.write('\x3Cscript type="text/javascript" src="' + ('https:' == document.location.protocol ? 'https://' : 'http://') + 'feed.mikle.com/js/rssmikle.js">\x3C/script>');</script><script type="text/javascript">(function() {var params = {rssmikle_url: "http://www.businessinsider.com/category/rss-feed",rssmikle_frame_width: "110%",rssmikle_frame_height: "500",frame_height_by_article: "3",rssmikle_target: "_blank",rssmikle_font: "Verdana, Arial",rssmikle_font_size: "12",rssmikle_border: "off",responsive: "off",rssmikle_css_url: "",text_align: "left",text_align2: "left",corner: "off",scrollbar: "on",autoscroll: "off",scrolldirection: "up",scrollstep: "3",mcspeed: "20",sort: "Off",rssmikle_title: "on",rssmikle_title_sentence: "",rssmikle_title_link: "",rssmikle_title_bgcolor: "#000000",rssmikle_title_color: "#FFFFFF",rssmikle_title_bgimage: "",rssmikle_item_bgcolor: "#FFFFFF",rssmikle_item_bgimage: "",rssmikle_item_title_length: "150",rssmikle_item_title_color: "#000000",rssmikle_item_border_bottom: "on",rssmikle_item_description: "on",item_link: "off",rssmikle_item_description_length: "750",rssmikle_item_description_color: "#666666",rssmikle_item_date: "gl1",rssmikle_timezone: "Etc/GMT",datetime_format: "%b %e, %Y %l:%M:%S %p",item_description_style: "text+tn",item_thumbnail: "full",item_thumbnail_selection: "auto",article_num: "15",rssmikle_item_podcast: "off",keyword_inc: "",keyword_exc: ""};feedwind_show_widget_iframe(params);})();</script><div style="font-size:10px; text-align:center; width:1000px;"></div><br/>
+
+    <script type="text/javascript">document.write('\x3Cscript type="text/javascript" src="' + ('https:' == document.location.protocol ? 'https://' : 'http://') + 'feed.mikle.com/js/rssmikle.js">\x3C/script>');</script><script type="text/javascript">(function() {var params = {rssmikle_url: "http://codinghorror.com",rssmikle_frame_width: "110%",rssmikle_frame_height: "500",frame_height_by_article: "3",rssmikle_target: "_blank",rssmikle_font: "Verdana, Arial",rssmikle_font_size: "12",rssmikle_border: "off",responsive: "off",rssmikle_css_url: "",text_align: "left",text_align2: "left",corner: "off",scrollbar: "on",autoscroll: "off",scrolldirection: "up",scrollstep: "3",mcspeed: "20",sort: "Off",rssmikle_title: "on",rssmikle_title_sentence: "",rssmikle_title_link: "",rssmikle_title_bgcolor: "#000000",rssmikle_title_color: "#FFFFFF",rssmikle_title_bgimage: "",rssmikle_item_bgcolor: "#FFFFFF",rssmikle_item_bgimage: "",rssmikle_item_title_length: "150",rssmikle_item_title_color: "#000000",rssmikle_item_border_bottom: "on",rssmikle_item_description: "on",item_link: "off",rssmikle_item_description_length: "750",rssmikle_item_description_color: "#666666",rssmikle_item_date: "gl1",rssmikle_timezone: "Etc/GMT",datetime_format: "%b %e, %Y %l:%M:%S %p",item_description_style: "text+tn",item_thumbnail: "full",item_thumbnail_selection: "auto",article_num: "15",rssmikle_item_podcast: "off",keyword_inc: "",keyword_exc: ""};feedwind_show_widget_iframe(params);})();</script><div style="font-size:10px; text-align:center; width:1000px;"></div><br/>
+
+    <script type="text/javascript">document.write('\x3Cscript type="text/javascript" src="' + ('https:' == document.location.protocol ? 'https://' : 'http://') + 'feed.mikle.com/js/rssmikle.js">\x3C/script>');</script><script type="text/javascript">(function() {var params = {rssmikle_url: "http://feeds.feedburner.com/allthingsgym",rssmikle_frame_width: "110%",rssmikle_frame_height: "500",frame_height_by_article: "3",rssmikle_target: "_blank",rssmikle_font: "Verdana, Arial",rssmikle_font_size: "12",rssmikle_border: "off",responsive: "off",rssmikle_css_url: "",text_align: "left",text_align2: "left",corner: "off",scrollbar: "on",autoscroll: "off",scrolldirection: "up",scrollstep: "3",mcspeed: "20",sort: "Off",rssmikle_title: "on",rssmikle_title_sentence: "",rssmikle_title_link: "",rssmikle_title_bgcolor: "#000000",rssmikle_title_color: "#FFFFFF",rssmikle_title_bgimage: "",rssmikle_item_bgcolor: "#FFFFFF",rssmikle_item_bgimage: "",rssmikle_item_title_length: "150",rssmikle_item_title_color: "#000000",rssmikle_item_border_bottom: "on",rssmikle_item_description: "on",item_link: "off",rssmikle_item_description_length: "750",rssmikle_item_description_color: "#666666",rssmikle_item_date: "gl1",rssmikle_timezone: "Etc/GMT",datetime_format: "%b %e, %Y %l:%M:%S %p",item_description_style: "text+tn",item_thumbnail: "full",item_thumbnail_selection: "auto",article_num: "15",rssmikle_item_podcast: "off",keyword_inc: "",keyword_exc: ""};feedwind_show_widget_iframe(params);})();</script><div style="font-size:10px; text-align:center; width:1000px;"></div><br/>
+
+    <script type="text/javascript">document.write('\x3Cscript type="text/javascript" src="' + ('https:' == document.location.protocol ? 'https://' : 'http://') + 'feed.mikle.com/js/rssmikle.js">\x3C/script>');</script><script type="text/javascript">(function() {var params = {rssmikle_url: "http://www.chess.com/rss/news",rssmikle_frame_width: "110%",rssmikle_frame_height: "500",frame_height_by_article: "3",rssmikle_target: "_blank",rssmikle_font: "Verdana, Arial",rssmikle_font_size: "12",rssmikle_border: "off",responsive: "off",rssmikle_css_url: "",text_align: "left",text_align2: "left",corner: "off",scrollbar: "on",autoscroll: "off",scrolldirection: "up",scrollstep: "3",mcspeed: "20",sort: "Off",rssmikle_title: "on",rssmikle_title_sentence: "",rssmikle_title_link: "",rssmikle_title_bgcolor: "#000000",rssmikle_title_color: "#FFFFFF",rssmikle_title_bgimage: "",rssmikle_item_bgcolor: "#FFFFFF",rssmikle_item_bgimage: "",rssmikle_item_title_length: "150",rssmikle_item_title_color: "#000000",rssmikle_item_border_bottom: "on",rssmikle_item_description: "on",item_link: "off",rssmikle_item_description_length: "750",rssmikle_item_description_color: "#666666",rssmikle_item_date: "gl1",rssmikle_timezone: "Etc/GMT",datetime_format: "%b %e, %Y %l:%M:%S %p",item_description_style: "text+tn",item_thumbnail: "full",item_thumbnail_selection: "auto",article_num: "15",rssmikle_item_podcast: "off",keyword_inc: "",keyword_exc: ""};feedwind_show_widget_iframe(params);})();</script><div style="font-size:10px; text-align:center; width:1000px;"></div><br/>
+
+    <script type="text/javascript">document.write('\x3Cscript type="text/javascript" src="' + ('https:' == document.location.protocol ? 'https://' : 'http://') + 'feed.mikle.com/js/rssmikle.js">\x3C/script>');</script><script type="text/javascript">(function() {var params = {rssmikle_url: "http://xkcd.com/rss.xml",rssmikle_frame_width: "110%",rssmikle_frame_height: "500",frame_height_by_article: "3",rssmikle_target: "_blank",rssmikle_font: "Verdana, Arial",rssmikle_font_size: "12",rssmikle_border: "off",responsive: "off",rssmikle_css_url: "",text_align: "left",text_align2: "left",corner: "off",scrollbar: "on",autoscroll: "off",scrolldirection: "up",scrollstep: "3",mcspeed: "20",sort: "Off",rssmikle_title: "on",rssmikle_title_sentence: "",rssmikle_title_link: "",rssmikle_title_bgcolor: "#000000",rssmikle_title_color: "#FFFFFF",rssmikle_title_bgimage: "",rssmikle_item_bgcolor: "#FFFFFF",rssmikle_item_bgimage: "",rssmikle_item_title_length: "150",rssmikle_item_title_color: "#000000",rssmikle_item_border_bottom: "on",rssmikle_item_description: "on",item_link: "off",rssmikle_item_description_length: "750",rssmikle_item_description_color: "#666666",rssmikle_item_date: "gl1",rssmikle_timezone: "Etc/GMT",datetime_format: "%b %e, %Y %l:%M:%S %p",item_description_style: "text+tn",item_thumbnail: "full",item_thumbnail_selection: "auto",article_num: "15",rssmikle_item_podcast: "off",keyword_inc: "",keyword_exc: ""};feedwind_show_widget_iframe(params);})();</script><div style="font-size:10px; text-align:center; width:1000px;"></div><br/>
+  </div>
 </body>
 </html>
